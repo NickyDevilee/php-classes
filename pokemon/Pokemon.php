78b7a8c221cbd691ce4212bd341476d831f28492
NickyDevilee <nickydevilee@hotmail.com>
2020-05-18 13:57:54 +0200

    kleine aanpassing
    attack functie

--- pokemon/Pokemon.php
+++ pokemon/Pokemon.php
@@ -53,30 +53,30 @@ abstract class Pokemon
 		if ($target->health <= 0) {
 			$target->health = 0;
 			echo "<br>";
-			echo $target->name."'s remaining health: ".$target->health.", use revive.<br>";
+			echo $target->getName()."'s remaining health: ".$target->health.", use revive.<br>";
 		} else {
 			$totaldamage = '';
 
-			if ($target->weakness->energyType->name == $this->energyType->name) {
+			if ($target->weakness->energyType->getName() == $this->energyType->getName()) {
 				$totaldamage = $attack->damage * $target->weakness->multiplier;
 			} else {
 				$totaldamage = $attack->damage;
-				if ($target->resistance->energyType->name == $this->energyType->name) {
+				if ($target->resistance->energyType->getName() == $this->energyType->getName()) {
 					$totaldamage = $attack->damage - $target->resistance->value;
 				}
 			}
 
-			echo $target->name."'s remaining health: ".$target->health."<br>";
+			echo $target->getName()."'s remaining health: ".$target->health."<br>";
 
-			echo 'Pokemon '.$this->name.' doet '.$attack->name.' en levert '.$totaldamage.' damage aan '.$target->name.'<br>';
+			echo 'Pokemon '.$this->getName().' doet '.$attack->getName().' en levert '.$totaldamage.' damage aan '.$target->getName().'<br>';
 
 			$target->health = $target->health - $totaldamage;
 
 			if ($target->health <= 0) {
 				$target->die();
-				echo $target->name." is dood.<br>";
+				echo $target->getName()." is dood.<br>";
 			} else {
-				echo $target->name."'s remaining health: ".$target->health."<br><br><br>";
+				echo $target->getName()."'s remaining health: ".$target->health."<br><br><br>";
 			}
 		}
 	}
